14f9a1646f2eb720af7c239275767edc71e1276a
Samuel <lee.eunseong7856@gmail.com>
2022-12-14 12:04:14 -0600

    commit tree 1a92b8c30dd3f02861acd443218872c954cebaf6

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,40 @@
-# your code goes here
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness 
+        @hygiene = hygiene
+    end
+
+    def name
+        @name
+    end
+
+    def bank_account
+        @bank_account
+    end
+
+    def happiness
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+        else
+            @happiness
+        end
+    end
+
+    def hygiene
+        if @hygiene > 10
+            @hygiene = 10
+        elsif @hygiene < 0
+            @hygiene = 0
+        else
+            @hygiene
+        end
+    end
+end
